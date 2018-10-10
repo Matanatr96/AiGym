3a5a820a316373de2f98b0e2847ac2c030ecf609
Anush Mattapalli <matanatr96@gmail.com>
2018-10-09 20:19:13 -0400

    changed environments, ran into issues

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import tensorflow
 import gym
 
-env = gym.make('CartPole-v0')
+env = gym.make('BipedalWalker-v2')
 for i_episode in range(20):
     observation = env.reset()
-    for t in range(10):
+    for t in range(1000):
         env.render()
         print(observation)
         action = env.action_space.sample()
+        print(action)
         observation, reward, done, info = env.step(action)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
